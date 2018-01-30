d51a02afc797ee7efdf7f0ecab71acd530298c0a
Gauthier Meert <Gauthier.Meert@dsoft.be>
2018-01-30 17:52:59 +0100

    Added brands carousel + navigation for main carousel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  ArrowLeft: string = "./../assets/paddle_prev_999.png";
+  ArrowRight: string = "./../assets/paddle_next_999.png";
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  fireHoverEvent($event) {
+    if($event.target.className == "center-left")
+    {
+      if ($event.type == "mouseleave") {
+        this.ArrowLeft = "./../assets/paddle_prev_999.png";
+      }
+      if ($event.type == "mouseenter") {
+        this.ArrowLeft = "./../assets/paddle_prev_fff.png";
+      }
+    }
+    else
+    {
+      if ($event.type == "mouseleave") {
+        this.ArrowRight = "./../assets/paddle_next_999.png";
+      }
+      if ($event.type == "mouseenter") {
+        this.ArrowRight = "./../assets/paddle_next_fff.png";
+      }
+    }
+  }
 }
